f60262af914ff113be140e1cf0482e4497653e3f
ryanml <rlanese@asu.edu>
2016-08-01 19:27:08 -0400

    Created constructor, is_equal_to method for Card()

--- setsolver/card.py
+++ setsolver/card.py
@@ -3,7 +3,21 @@
 class Card(object):
 
     """
-    Constructor
+    Constructor takes in values for for dimensions:
+    Color, Number, Shape, and Shading
     """
-    def __init__(self):
-        pass
+    def __init__(self, color, number, shape, shading):
+        self.dims = {}
+        self.dims['color'] = color
+        self.dims['number'] = number
+        self.dims['shape'] = shape
+        self.dims['shading'] = shading
+
+    """
+    The is_equal_to compares the dimensions of one
+    card to another
+    Params: self, card (Card object)
+    Returns: boolean
+    """
+    def is_equal_to(self, card):
+        return self.dims == card.dims
--- setsolver/tests.py
+++ setsolver/tests.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python
+from card import Card
+from solver import Solver
 import unittest
 
 class SetSolverTests(unittest.TestCase):
 
     """
-    Checks to make sure bar is true
+    Tests the Card().is_equal function (Outcome: True)
     """
-    def test_assert(self):
-        bar = True
-        self.assertTrue(bar)
+    def test_is_equal_equal(self):
+        card_one = Card('Red', 'Solid', 'Diamond', 3)
+        card_two = Card('Red', 'Solid', 'Diamond', 3)
+        self.assertEqual(card_one.is_equal_to(card_two), True)
+
+    """
+    Tests the Card().is_equal function (Outcome: False)
+    """
+    def test_is_equal_not_equal(self):
+        card_one = Card('Red', 'Solid', 'Diamond', 3)
+        card_two = Card('Red', 'Solid', 'Diamond', 2)
+        self.assertEqual(card_one.is_equal_to(card_two), False)
 
 if __name__ == '__main__':
     unittest.main()
